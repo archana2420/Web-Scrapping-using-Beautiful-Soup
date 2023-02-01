8add49e2bf0c9bd3c8d4c65be01118f726479231
Archana S Nair-ENG18CS0044 <65852760+archana2420@users.noreply.github.com>
2023-02-01 16:18:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,34 +8,15 @@ htmlContent = r.content
 
 # Creating Soup(Parse the HTML)
 soup = BeautifulSoup(htmlContent,'html.parser')
-# print(soup)
-# HTML Tree traversal
-# title_tag = soup.title
-# print(meta_tag)
 
 companies=soup.findAll('h3',class_="joblist-comp-name")
-# print("Company Names")
+
 for i in companies:
     print("-----------------------------------------------")
     print("Company Name:",i.get_text())
     info=soup.find('li',class_="clearfix job-bx wht-shd-bx")
     skills = info.findAll('span', class_="srp-skills")
-    # location=info.findAll('ul',class_="top-jd-dtl clearfix")
+   
     for i in skills:
         print("Skills:",i.get_text())
-    # city=location.findAll('span')
-    # print(locations)
-    # for i in location['span']:
-    #     print(i['title'])
-
-    # print(info.find('span',class_="srp-skills").get_text)
-# skills=soup.findAll('span',class_="srp-skills")
-# print("Skills :")
-# for i in skills:
-#     print(i.get_text())
-# info=soup.findAll('ul',class_="new-joblist")
-# # locations=info.findAll('li')
-# city=info.findAll('span')
-# # print(locations)
-# for i in city:
-#     print(i['title'])
+    
